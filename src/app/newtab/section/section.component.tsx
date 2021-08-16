19c2b30c09c26a200221cd91fc4acd62d11ecbcb
Yao <yaowang831@gmail.com>
2021-08-16 19:46:32 -0400

    remove border from last section element

--- src/app/newtab/section/section.component.tsx
+++ src/app/newtab/section/section.component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Section:React.FC = props => {
 
   return (
-    <div className="w-full my-t border-r-2 border-blue-800 px-2">
+    <div className="w-full my-t border-r-2 last:border-0 border-blue-800 px-2">
       {props.children}
     </div>
   )
