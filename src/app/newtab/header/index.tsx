6be1589c8b4b738ff059eb76d5ab0e694919651b
Yao <yaowang831@gmail.com>
2021-08-16 15:08:59 -0400

    updated header

--- src/app/newtab/header/index.tsx
+++ src/app/newtab/header/index.tsx
@@ -5,7 +5,9 @@ import ReactDOM from "react-dom";
 export const Header = () => {
 
   return (
-    <div>NEW TAB IMAGE</div>
+    <div className="w-full border-b-2 border-white">
+      <div className="text-4xl text-white">Life is too short!</div>
+    </div>
   )
 };
 
