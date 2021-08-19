ad3700d2b81958b4264b31aa875f96e1b9ef6a26
Yao <yaowang831@gmail.com>
2021-08-18 21:04:16 -0400

    show top sites when init

--- src/app/newtab/tabs/link/link.interfaces.ts
+++ src/app/newtab/tabs/link/link.interfaces.ts
@@ -2,6 +2,6 @@
 export interface LinkProps {
   id: string,
   link: string | undefined;
-  imageUrl: string | undefined;
+  imageUrl?: string | undefined;
   title: string | undefined;
 }
--- src/app/newtab/topsites/topsites.component.tsx
+++ src/app/newtab/topsites/topsites.component.tsx
@@ -1,30 +1,51 @@
+import { nanoid } from 'nanoid';
 import React from 'react'
 
+import Row from '../tabs/row/row.component';
 import Section from '../section/section.component';
+import { LinkProps } from '../tabs/link/link.interfaces';
 
-interface TopSiteProps {
+const TopSites:React.FC = () => {
+  const [topSites, setTopSites] = React.useState<LinkProps[] | undefined>(undefined);
 
-}
+  // const clickHandler = () => {
+  //   console.log(chrome);
+  //   console.log(chrome.topSites);
+  //   chrome.topSites.get((url_list) => {
+  //     for(var i=0;i<url_list.length;i++) {
+  //       console.log(url_list[i]);
+  //     }
+  //   })
+  // }
 
-const TopSites:React.FC<TopSiteProps> = ({}:TopSiteProps) => {
-  // const [topSites, setTopSites] = React.useState(undefined);
-
-  const clickHandler = () => {
-    console.log(chrome);
-    console.log(chrome.topSites);
-    chrome.topSites.get((url_list) => {
-      for(var i=0;i<url_list.length;i++) {
-        console.log(url_list[i]);
-      }
-    })
-  }
+  React.useEffect(() => {
+    if(chrome.topSites) {
+      chrome.topSites.get((url_list) => {
+        const formatData: LinkProps[] = [];
+        url_list.map((x) => {
+          formatData.push({
+            id: nanoid(),
+            link: x.url,
+            title: x.title
+          })
+        })
+        setTopSites(formatData)
+      })
+    } else {
+      console.error('Your browser doesn\'t support top site method!');
+    }
+  }, [])
 
 
   return (
     <Section>
-      <div className="grid grid-cols-3 mb-4">
-        <button onClick={clickHandler}>get top sites</button>      
-      </div>
+      <h3 className="text-md font-bold border-b-2 border-white text-white mb-2">Top Sites</h3>
+      {/* <button onClick={clickHandler}>get top sites</button>    */}
+      {
+        topSites ?
+        <Row contentArr={topSites}/> :
+        ''
+      }
     </Section>
   )
 }
