2b734c47a6928a4edfe639cc9025aff1af518d57
Yao <yaowang831@gmail.com>
2021-08-19 22:01:56 -0400

    close all other tabs

--- src/app/newtab/header/index.tsx
+++ src/app/newtab/header/index.tsx
@@ -70,7 +70,16 @@ export const Header = () => {
   }
 
   const closeClickHandler = () => {
-    //TODO: close other tabs
+    let queryOptions = {};
+    chrome.tabs.query(queryOptions).then((res) => {
+      const tabIdArr: number[] = res?.filter( r => r)?.filter(x => !x.active)?.map(i => i.id ? i.id : 0 ) || [];
+      // console.log(tabIdArr);
+      if(tabIdArr) {
+        chrome.tabs.remove(tabIdArr.filter(x => x)).then((res) => {
+          console.log('Closed tabs')
+        })
+      }
+    })
   }
 
   const resetClickHandler = () => {
