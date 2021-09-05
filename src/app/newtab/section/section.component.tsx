0b264765192d50088f1c497093193cf566d30533
Yao <yaowang831@gmail.com>
2021-09-05 17:51:36 -0400

    fixed section height scroll problem

--- src/app/newtab/section/section.component.tsx
+++ src/app/newtab/section/section.component.tsx
@@ -8,7 +8,7 @@ const Section:React.FC = props => {
   const colorTheme = useRecoilValue<'blueTheme' | 'blackTheme' | 'whiteTheme'>(colorThemeSelector);
 
   return (
-    <div className={`relative w-full my-t border-r-2 last:border-0 ${setting.border[colorTheme]} ${setting.text[colorTheme]} px-2 mt-4`}>
+    <div className={`nt-section relative w-full my-t border-r-2 last:border-0 ${setting.border[colorTheme]} ${setting.text[colorTheme]} px-2 mt-4`}>
       {props.children}
     </div>
   )
