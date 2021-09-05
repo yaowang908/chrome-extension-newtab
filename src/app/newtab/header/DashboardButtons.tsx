0b6839e80ec9f5db164ad6f441ccaa01ed41d675
Yao <yaowang831@gmail.com>
2021-09-04 20:33:26 -0400

    separate dashboard buttons into standalone component

---
+++ src/app/newtab/header/DashboardButtons.tsx
@@ -0,0 +1,173 @@
+import React from 'react'
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
+import { nanoid } from "nanoid";
+
+import { linksSelector } from "../Recoil/links_selector.atom";
+import { colorThemeSelector } from "../Recoil/color_theme.atom";
+import { LinkProps } from "../tabs/link/link.interfaces";
+import { groupSelector } from "../Recoil/group_selector.atom";
+import setting from "../setting/setting";
+import { visibleSelector } from "../Recoil/visible.atom";
+import { settingDialogueVisibility } from "../Recoil/setting.atom";
+import { viewSelector } from "../Recoil/view.atom";
+
+const DashboardButtons = () => {
+  const [dataArr, setDataArr] = useRecoilState(linksSelector);
+  const resetLinks = useResetRecoilState(linksSelector);
+  const resetGroups = useResetRecoilState(groupSelector);
+  const colorTheme = useRecoilValue(colorThemeSelector);
+  const [visible, setVisible] = useRecoilState(visibleSelector);
+  const [viewState, setViewState] = useRecoilState(viewSelector);
+  const [settingVisibility, setSettingVisibility] = useRecoilState(
+    settingDialogueVisibility
+  );
+  
+  
+  const collectClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    let queryOptions = {};
+
+    const getOrder = (
+      prevArr: LinkProps[] | undefined,
+      currentIndex: number
+    ) => {
+      if (prevArr) {
+        return prevArr.length + currentIndex;
+      }
+      return currentIndex;
+    };
+
+    chrome.tabs.query(queryOptions).then((res) => {
+      // console.log(res);
+      const formatData = res
+        .filter((x) => x?.url && !x?.url.includes("chrome://"))
+        .map((x, index) => ({
+          id: index + "_" + nanoid(),
+          index: getOrder(dataArr, index),
+          imageUrl: x?.favIconUrl,
+          link: x?.url,
+          title: x?.title,
+          priority: 0,
+        }));
+      if (dataArr) {
+        const newState = [...dataArr, ...formatData];
+        if (hasDuplicates(newState)) {
+          console.log("Removed duplicates!");
+          setDataArr(removeDuplicates(newState));
+        } else {
+          setDataArr(newState);
+        }
+        // setDataArr([...dataArr, ...formatData]);
+      } else {
+        setDataArr(formatData);
+      }
+    });
+    // chrome.runtime.sendMessage({
+    //   method: "allTabs"
+    // }, (response) => {
+    //   console.log(response)
+    // });
+    closeClickHandler();
+  };
+
+  const removeDuplicates = (arr: LinkProps[]) => {
+    return arr.filter(
+      (value, index, array) =>
+        array.findIndex((t) => t.link === value.link) === index
+    );
+  };
+
+  const hasDuplicates = (arr: LinkProps[]) => {
+    const uniqueUrls = new Set(arr.map((x) => x.link));
+    if (uniqueUrls.size < arr.length) {
+      return true;
+    }
+    return false;
+  };
+
+  const closeClickHandler = () => {
+    let queryOptions = {};
+    chrome.tabs.query(queryOptions).then((res) => {
+      const tabIdArr: number[] =
+        res
+          ?.filter((r) => r)
+          ?.filter((x) => !x.active)
+          ?.map((i) => (i.id ? i.id : 0)) || [];
+      // console.log(tabIdArr);
+      if (tabIdArr) {
+        chrome.tabs.remove(tabIdArr.filter((x) => x)).then((res) => {
+          console.log("Closed tabs");
+        });
+      }
+    });
+  };
+
+  const openAllClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    dataArr?.map((link, index) => {
+      if (!index) {
+        chrome.tabs.create({
+          active: true,
+          url: link.link,
+        });
+      }
+      chrome.tabs.create({
+        active: false,
+        url: link.link,
+      });
+    });
+  };
+
+  const resetClickHandler = () => {
+    if (window.confirm("Are you sure about delete all saved tabs?")) {
+      chrome.storage.local.remove(["tabs", "groups"], function () {
+        let error = chrome.runtime.lastError;
+        if (error) {
+          console.error(error);
+        }
+      });
+      resetLinks();
+      resetGroups();
+    } else {
+      console.log("Abort!");
+    }
+  };
+  
+  const settingClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setSettingVisibility(!settingVisibility);
+  };
+
+  return (
+    <div
+      className={`w-full sm:w-96 grid grid-cols-4 gap-2 mb-4 ${setting.text[colorTheme]}`}
+    >
+      <button
+        onClick={settingClickHandler}
+        className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
+      >
+        Setting
+      </button>
+      <button
+        onClick={collectClickHandler}
+        className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
+      >
+        Collect
+      </button>
+      <button
+        onClick={openAllClickHandler}
+        className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
+      >
+        Open all
+      </button>
+      <button
+        onClick={resetClickHandler}
+        className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg cols-span-2 hover:text-red-900 hover:border-red-900`}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default DashboardButtons
--- src/app/newtab/header/index.tsx
+++ src/app/newtab/header/index.tsx
@@ -4,17 +4,15 @@ import {
   useRecoilValue,
   useResetRecoilState,
 } from "recoil";
-import { nanoid } from "nanoid";
 
 import { linksSelector } from '../Recoil/links_selector.atom';
-import { LinkProps } from "../tabs/link/link.interfaces";
 import { colorThemeSelector } from "../Recoil/color_theme.atom";
 import { groupSelector } from '../Recoil/group_selector.atom';
 import setting from '../setting/setting';
 import { visibleSelector } from '../Recoil/visible.atom';
-import { settingDialogueVisibility } from "../Recoil/setting.atom";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch.component";
 import { viewSelector } from '../Recoil/view.atom';
+import DashboardButtons from "./DashboardButtons";
 
 export const Header = () => {
   const [dataArr, setDataArr] = useRecoilState(linksSelector);
@@ -23,9 +21,6 @@ export const Header = () => {
   const colorTheme = useRecoilValue(colorThemeSelector);
   const [visible, setVisible] = useRecoilState(visibleSelector);
   const [viewState, setViewState] = useRecoilState(viewSelector);
-  const [settingVisibility, setSettingVisibility] = useRecoilState(
-    settingDialogueVisibility
-  );
 
   React.useEffect(() => {
     chrome.storage.local.get(['tabs'], function(result) {
@@ -52,109 +47,6 @@ export const Header = () => {
     });
   }, []);
 
-  const collectClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    let queryOptions = {};
-
-    const getOrder = (
-      prevArr: LinkProps[] | undefined,
-      currentIndex: number
-    ) => {
-      if (prevArr) {
-        return prevArr.length + currentIndex;
-      }
-      return currentIndex;
-    };
-
-    chrome.tabs.query(queryOptions).then((res) => {
-      // console.log(res);
-      const formatData = res
-        .filter((x) => x?.url && !x?.url.includes("chrome://"))
-        .map((x, index) => ({
-          id: index + "_" + nanoid(),
-          index: getOrder(dataArr, index),
-          imageUrl: x?.favIconUrl,
-          link: x?.url,
-          title: x?.title,
-          priority: 0,
-        }));
-      if (dataArr) {
-        const newState = [...dataArr, ...formatData];
-        if (hasDuplicates(newState)) {
-          console.log("Removed duplicates!");
-          setDataArr(removeDuplicates(newState));
-        } else {
-          setDataArr(newState);
-        }
-        // setDataArr([...dataArr, ...formatData]);
-      } else {
-        setDataArr(formatData);
-      }
-    });
-    // chrome.runtime.sendMessage({
-    //   method: "allTabs"
-    // }, (response) => {
-    //   console.log(response)
-    // });
-    closeClickHandler();
-  };
-
-  const removeDuplicates = (arr:LinkProps[]) => {
-    return arr.filter((value, index, array) => array.findIndex(t => (t.link === value.link)) === index);
-  }
-
-  const hasDuplicates = (arr:LinkProps[]) => {
-    const uniqueUrls = new Set(arr.map(x => x.link));
-    if (uniqueUrls.size < arr.length) {
-      return true;
-    }
-    return false;
-  }
-
-  const closeClickHandler = () => {
-    let queryOptions = {};
-    chrome.tabs.query(queryOptions).then((res) => {
-      const tabIdArr: number[] = res?.filter( r => r)?.filter(x => !x.active)?.map(i => i.id ? i.id : 0 ) || [];
-      // console.log(tabIdArr);
-      if(tabIdArr) {
-        chrome.tabs.remove(tabIdArr.filter(x => x)).then((res) => {
-          console.log('Closed tabs')
-        })
-      }
-    })
-  }
-
-  const openAllClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    dataArr?.map((link, index) => {
-      if (!index) {
-        chrome.tabs.create({
-          active: true,
-          url: link.link,
-        });
-      }
-      chrome.tabs.create({
-        active: false,
-        url: link.link,
-      });
-    });
-  };
-
-  const resetClickHandler = () => {
-    if(window.confirm("Are you sure about delete all saved tabs?")) {
-      chrome.storage.local.remove(["tabs", "groups"], function() {
-        let error = chrome.runtime.lastError;
-        if(error) {
-          console.error(error)
-        }
-      });
-      resetLinks();
-      resetGroups();
-    } else {
-      console.log("Abort!")
-    }
-  }
-
   const headerClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     console.log("Single Click!");
     e.stopPropagation();
@@ -167,16 +59,11 @@ export const Header = () => {
     setVisible(!visible);
   };
 
-  const settingClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    setSettingVisibility(!settingVisibility);
-  };
-
   const viewClickHandler = (selectedName: ('Dashboard' | 'Bookmark')) => {
     // console.log(selectedName);
     setViewState(selectedName)
   }
-
+  // TODO: hide buttons when in bookmark view
   return (
     <div
       className={`flex-initial w-full border-b-2 ${
@@ -191,38 +78,11 @@ export const Header = () => {
         <ToggleSwitch
           defaultName="Dashboard"
           optionName="Bookmark"
+          selectedPosition={viewState === "Dashboard" ? 0 : 1}
           onChange={viewClickHandler}
         />
       </div>
-
-      <div
-        className={`w-full sm:w-96 grid grid-cols-4 gap-2 mb-4 ${setting.text[colorTheme]}`}
-      >
-        <button
-          onClick={settingClickHandler}
-          className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
-        >
-          Setting
-        </button>
-        <button
-          onClick={collectClickHandler}
-          className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
-        >
-          Collect
-        </button>
-        <button
-          onClick={openAllClickHandler}
-          className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg`}
-        >
-          Open all
-        </button>
-        <button
-          onClick={resetClickHandler}
-          className={`${setting.text[colorTheme]} ${setting.headBorder[colorTheme]} text-lg cols-span-2 hover:text-red-900 hover:border-red-900`}
-        >
-          Reset
-        </button>
-      </div>
+      {viewState === "Dashboard" ? <DashboardButtons /> : ""}
     </div>
   );
 };
