6fa21b82a2fb42839e9ae80ad553a538e9db4d1b
Yao <yaowang831@gmail.com>
2021-09-12 16:30:07 -0400

    fix typscript error

--- src/app/newtab/section/section.component.tsx
+++ src/app/newtab/section/section.component.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useRecoilValue } from "recoil";
 
-import { colorThemeSelector } from "../Recoil/color_theme.atom";
+import { colorThemeSelector, colorThemeProp } from "../Recoil/color_theme.atom";
 import setting from '../setting/setting';
 
 const Section:React.FC = props => {
-  const colorTheme = useRecoilValue<'blueTheme' | 'blackTheme' | 'whiteTheme'>(colorThemeSelector);
+  const colorTheme = useRecoilValue<colorThemeProp>(colorThemeSelector);
 
   return (
     <div className={`nt-section relative w-full my-t border-r-2 last:border-0 ${setting.border[colorTheme]} ${setting.text[colorTheme]} px-2 mt-4`}>
