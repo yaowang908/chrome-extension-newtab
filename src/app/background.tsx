31a3c322d90803030a237403c0969b7af1b7394a
Yao <yaowang831@gmail.com>
2021-09-12 15:39:21 -0400

    button onclick open new tab page

--- src/app/background.tsx
+++ src/app/background.tsx
@@ -1,26 +1,5 @@
-// chrome.storage.local.get(["badgeText"], ({ badgeText }) => {
-//   chrome.action.setBadgeText({ text: badgeText }, () => {});
+// console.log("====background js====");
 
-//   // Listener is registered asynchronously
-//   chrome.action.onClicked.addListener(handleActionClick);
-// });
-
-// const handleActionClick = () => console.log("background js");
-
-console.log("====background js====");
-
-let queryOptions = {};
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("get request for allTabs")
-    if (request.method === "allTabs") {
-      // chrome.tabs.query(queryOptions).then((res) => {
-      //   // console.log(res);
-      //   sendResponse(JSON.stringify(res));
-      // });
-    } else {
-      console.log("Unknown method!", request.method);
-    }
-  }
-);
+chrome.action.onClicked.addListener(function () {
+  chrome.tabs.create({url: chrome.runtime.getURL("new-tab.html")})
+});
